13f21cd8736d6036d497a351f0f692947474e67e
gluke0 <milanesigianluca@gmail.com>
2023-06-01 14:45:06 +0200

    header e footer

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once __DIR__ . '/views/partials/head.php';
+
+include_once __DIR__ . '/views/partials/footer.php';
+
+?>
---
+++ views/partials/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="container d-flex justify-content-center">
+        <span>
+            <p class="font-italic"> made with <span style="font-size:123%;color:red;">&hearts;</span> by someone in 2023</p>
+        </span>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
